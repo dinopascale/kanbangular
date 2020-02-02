46c20c269f6d69903341c64e51beb06f1fc14eb8
Dino Pascale <dino.pascale@gmail.com>
2020-02-02 13:47:38 +0100

    modified package.json for now dev and testing serverless api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kanbangular';
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    // console.log(this.baseUrl);
+
+    this._http.get('/api/greet').subscribe()
+  }
+
 }
